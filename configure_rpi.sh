2fabeba73283f1820d1ceb9ee62c88550f0af8e4
Затолокин Павел <Zatolokin.Pavel@gmail.com>
2021-09-05 21:31:47 +0300

    Update configure_rpi.sh

--- configure_rpi.sh
+++ configure_rpi.sh
@@ -5,4 +5,11 @@
 
 echo "Raspberry Pi configuration starts"
 
+sudo apt-get update
+sudo apt-get dist-upgrade
+sudo apt-get upgrade
+sudo apt-get clean
+sudo apt-get autoremove
+sudo apt-get autoclean
+
 sudo apt-get install vim mc git htop
