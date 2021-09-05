b05cb149b2af05688f9bf0cf55f3b945118b8e1e
Затолокин Павел <Zatolokin.Pavel@gmail.com>
2021-09-05 21:04:38 +0300

    Create configure_rpi.sh

---
+++ configure_rpi.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Скрипт настройки Raspberry Pi с нуля. Устанавливает и настраивает всё то, что можно сделать через скрипт.
+# Использовать согласно инструкции https://github.com/ZatolokinPavel/RPiNotes
+
+echo "Raspberry Pi configuration starts"
+
+sudo apt-get install vim mc git htop
