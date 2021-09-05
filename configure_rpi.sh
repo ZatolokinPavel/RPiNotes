4e8546967ae8e5bba837e8179782c0a14446e663
Затолокин Павел <Zatolokin.Pavel@gmail.com>
2021-09-05 22:42:07 +0300

    Update configure_rpi.sh

--- configure_rpi.sh
+++ configure_rpi.sh
@@ -13,3 +13,17 @@ sudo apt-get autoremove
 sudo apt-get autoclean
 
 sudo apt-get install vim mc git htop
+
+# Nginx
+if [[ $(lsb_release -cs) = buster ]]
+then
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo echo "deb http://raspbian.raspberrypi.org/raspbian/ bullseye main contrib non-free rpi" | sudo tee -a /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install nginx
+sudo rm /etc/apt/sources.list
+sudo mv /etc/apt/sources.list.bak /etc/apt/sources.list
+sudo apt-get update
+else
+sudo apt-get install nginx
+fi
