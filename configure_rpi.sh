b6335aadd39717cbdd9fb0a281a570214621d941
Затолокин Павел <Zatolokin.Pavel@gmail.com>
2021-09-13 13:44:27 +0300

    Update configure_rpi.sh

--- configure_rpi.sh
+++ configure_rpi.sh
@@ -26,18 +26,7 @@ crudini --set /root/.config/mc/ini Panels navigate_with_arrows true
 crudini --set /root/.config/mc/ini Midnight-Commander skin dark
 
 # Nginx
-if [[ $(lsb_release -cs) = buster ]]
-then
-sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
-sudo echo "deb http://raspbian.raspberrypi.org/raspbian/ bullseye main contrib non-free rpi" | sudo tee -a /etc/apt/sources.list
-sudo apt-get update
 sudo apt-get install nginx
-sudo rm /etc/apt/sources.list
-sudo mv /etc/apt/sources.list.bak /etc/apt/sources.list
-sudo apt-get update
-else
-sudo apt-get install nginx
-fi
 sudo git clone https://github.com/ZatolokinPavel/nginx.git /srv/nginx
 sudo mkdir /srv/logs
 sudo rm /etc/nginx/sites-enabled/default
