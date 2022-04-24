c827aec81e1ece7c1a8f1c8785f257e3a251c54f
Затолокин Павел <Zatolokin.Pavel@gmail.com>
2022-04-24 16:45:08 +0300

    Update configure_rpi.sh

--- configure_rpi.sh
+++ configure_rpi.sh
@@ -90,7 +90,6 @@ sudo mkdir -p /var/www/html/.well-known/acme-challenge
 # site okfilm.com.ua
 sudo git clone https://github.com/ZatolokinPavel/okfilm_2018.git /srv/okfilm_2018
 sudo mkdir /srv/cdn
-#sudo mkdir /srv/shared-global
 sudo mkdir /mnt/okdisk/shared-global
 sudo chmod 777 /mnt/okdisk/shared-global
 sudo ln -s /mnt/okdisk/shared-global/ /srv/shared-global
